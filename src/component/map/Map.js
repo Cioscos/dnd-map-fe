b6599ad929ef1e1adf989deaf61ee3c43fbaad4b
Cioscos <claudiocicca1998@gmail.com>
2023-05-14 20:04:32 +0200

    Fixed test hex map

--- src/component/map/Map.js
+++ src/component/map/Map.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import {GridGenerator, Hexagon, HexGrid, Layout} from 'react-hexgrid';
+import {GridGenerator, Hexagon, HexGrid, Layout, Text} from 'react-hexgrid';
 import './css/Map.css'; // We will create this later
 
 function Map() {
     // Let's assume that we have 5x5 grid for simplicity
-    const hexagons = GridGenerator.rectangle(20, 40);
+    const hexagons = GridGenerator.rectangle(10, 10);
 
     return (
         <div className="map">
             <h1>This is the Map!</h1>
-            <HexGrid width={800} height={600}>
-                <Layout size={{x: 1000, y: 800}} flat={false} spacing={1.02} origin={{x: -45, y: -15}}>
+            <HexGrid width={1000} height={800}>
+                <Layout size={{x: 6, y: 6}} flat={false} spacing={1.02} origin={{x: -45, y: -40}}>
                     {
                         hexagons.map((hex, i) => (
-                            <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s}/>
+                            <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s}>
+                                <Text fontSize="5" color={'#ff0000'}>{`${hex.q}-${hex.r}`}</Text>
+                            </Hexagon>
                         ))
                     }
                 </Layout>
             </HexGrid>
         </div>
     );
-
 }
 
 export default Map;
